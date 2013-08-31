97471e77ddebec823a259987564e7a9e281c2911
Aubrey Howell <simplyaubs@gmail.com>
2013-08-31 12:19:12 -0600

    Added all the things

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Suggestotron::Application.config.secret_key_base = '8d6ff253595d3cf9650ba02c3c21457e98577088f94357d984e67a2cc8b941234f2089021ed6703660024141242ad6282f98ec1ae5c8f9c5ecbcd5ad72110f08'
